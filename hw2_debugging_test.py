e29f137ee06df74135b75c054ef49e4406e995cc
shaziamuckram <158780766+shaziamuckram@users.noreply.github.com>
2024-09-05 17:02:19 +0000

    Testing merge sort

--- hw2_debugging_test.py
+++ hw2_debugging_test.py
@@ -2,10 +2,14 @@ import pytest
 
 from hw2_debugging import merge_sort
 
-def test_mergesort_random_pass():
+def test_mergesort_random():
     A = [89, 67, 23, 12, 45, 100]
     B = [12, 23, 45, 67, 89, 100]
     assert(merge_sort(A)) == B
 
-# def test_mergesort_fail():
+def test_mergesort_reverse():
+    A = [100, 45, 23, 21, 20, 15, 9, 5, 2, 1]
+    B = [1, 2, 5, 9, 15, 20, 21, 23, 45, 100]
+    assert(merge_sort(A)) == B
+
     
