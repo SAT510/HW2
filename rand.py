cf7df27d38bc21715e0448a172038fa7cc99623f
Amay Gada <61084829+amaygada@users.noreply.github.com>
2024-09-05 15:33:39 +0000

    pylint done

--- rand.py
+++ rand.py
@@ -1,7 +1,19 @@
+"""
+This module handles generation of random numbers
+"""
+
 import secrets
 
-
 def random_array(arr):
-    for i in range(len(arr)):
+    """
+    Replace each element in the array with a random number between 1 and 20.
+    
+    Args:
+        arr (list): The input array to be filled with random numbers.
+    
+    Returns:
+        list: The modified array with random numbers.
+    """
+    for i, _ in enumerate(arr):
         arr[i] = secrets.randbelow(20) + 1
     return arr
