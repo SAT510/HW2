ee46401c86f31f876a1fd6ad318f6d2c224af8b8
Amay Gada <61084829+amaygada@users.noreply.github.com>
2024-09-11 20:54:15 +0000

    fixed pylint suggestions on the tes.py file

--- hw2_debugging_test.py
+++ hw2_debugging_test.py
@@ -1,21 +1,32 @@
-import pytest
+"""
+Tests for mergesort
+"""
 
 from hw2_debugging import merge_sort
 
 
 def test_mergesort_random():
-    A = [89, 67, 23, 12, 45, 100]
-    B = [12, 23, 45, 67, 89, 100]
-    assert (merge_sort(A)) == B
+    """
+    Test 1 for mergesort
+    """
+    a = [89, 67, 23, 12, 45, 100]
+    b = [12, 23, 45, 67, 89, 100]
+    assert (merge_sort(a)) == b
 
 
 def test_mergesort_reverse():
-    C = [100, 45, 23, 21, 20, 15, 9, 5, 2, 1]
-    D = [1, 2, 5, 9, 15, 20, 21, 23, 45, 100]
-    assert (merge_sort(C)) == D
+    """
+    Test 2 for mergesort
+    """
+    c = [100, 45, 23, 21, 20, 15, 9, 5, 2, 1]
+    d = [1, 2, 5, 9, 15, 20, 21, 23, 45, 100]
+    assert (merge_sort(c)) == d
 
 
 def test_mergesort_negative():
-    E = [-50, -90, -45, -9, -1, -43, -35, -76, -63]
-    F = [-90, -76, -63, -50, -45, -43, -35, -9, -1]
-    assert (merge_sort(E)) == F
+    """
+    Test 3 for mergesort
+    """
+    e = [-50, -90, -45, -9, -1, -43, -35, -76, -63]
+    f = [-90, -76, -63, -50, -45, -43, -35, -9, -1]
+    assert (merge_sort(e)) == f
