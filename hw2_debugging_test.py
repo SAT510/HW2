92781993a9ef83aafea4b6ae4024a35f5cded57f
shaziamuckram <158780766+shaziamuckram@users.noreply.github.com>
2024-09-28 23:32:13 +0000

    Testing action failure

--- hw2_debugging_test.py
+++ hw2_debugging_test.py
@@ -11,7 +11,9 @@ def test_mergesort_random():
     """
     a = [89, 67, 23, 12, 45, 100]
     b = [12, 23, 45, 67, 89, 100]
-    assert (merge_sort(a)) == b
+    result = merge_sort(b)
+    if result != b:
+        raise ValueError(f"Expected {b}, but got {result}")
 
 
 def test_mergesort_reverse():
@@ -20,7 +22,10 @@ def test_mergesort_reverse():
     """
     c = [100, 45, 23, 21, 20, 15, 9, 5, 2, 1]
     d = [1, 2, 5, 9, 15, 20, 21, 23, 45, 100]
-    assert (merge_sort(c)) == d
+    result = merge_sort(d)
+    if result != d:
+        raise ValueError(f"Expected {d}, but got {result}")
+
 
 
 def test_mergesort_negative():
@@ -29,4 +34,6 @@ def test_mergesort_negative():
     """
     e = [-50, -90, -45, -9, -1, -43, -35, -76, -63]
     f = [-90, -76, -63, -50, -45, -43, -35, -9, -1]
-    assert (merge_sort(e)) == f
+    result = merge_sort(e)
+    if result != f:
+        raise ValueError(f"Expected {f}, but got {result}")
